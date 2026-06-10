c6cb173abb6fba8fad745da8c99de8323a0d0a89
rif42 <rifkyariya42@gmail.com>
2026-06-10 10:56:07 +0700

    ecosystem map + text done

--- src/data/locations.ts
+++ src/data/locations.ts
@@ -6,9 +6,10 @@ export interface CampusLocation {
 
 export const campusLocations: CampusLocation[] = [
   { name: "Kampus Utama APU", lat: -7.071300, lng: 110.362306 },
-  { name: "SCCR", lat: -7.070469, lng: 110.358658 },
   { name: "Karenina Resort", lat: -7.069780, lng: 110.359637 },
   { name: "Dermanina", lat: -7.069804, lng: 110.359198 },
+  { name: "SCCR", lat: -7.070469, lng: 110.358658 },
   { name: "Agung Farm", lat: -7.075039, lng: 110.358688 },
   { name: "Sains De Resto", lat: -7.074095, lng: 110.359329 },
+  { name: "Kampus Utama APU", lat: -7.071300, lng: 110.362306 },
 ];
